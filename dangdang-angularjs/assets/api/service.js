786ed719bbd25f7e8e917478e8d06eeec50b5590
nill0801 <879056126@qq.com>
2016-10-19 15:21:33 +0800

    updata

---
+++ dangdang-angularjs/assets/api/service.js
@@ -0,0 +1,16 @@
+app.factory('commonService',['$http',function($http){
+  var service={};
+  service.getDate=function(type,callBack){
+    $http({
+      url:'../../data/book_'+type+'.json',
+      method:'get'
+    })
+    .then(function(res){
+      console.log('获取数据成功');
+      callBack(res);
+    },function(err){
+      console.log(err);
+    });
+  };
+  return service;
+}]);
