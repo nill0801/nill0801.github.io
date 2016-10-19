fabb5e565ebc2919798bd9234d5ac3f58aead553
nill0801 <879056126@qq.com>
2016-10-19 16:27:02 +0800

    e

--- dangdang-angularjs/assets/api/service.js
+++ dangdang-angularjs/assets/api/service.js
@@ -1,16 +1,16 @@
-app.factory('commonService',['$http',function($http){
-  var service={};
-  service.getDate=function(type,callBack){
-    $http({
-      url:'/dangdang-angularjs/data/book_'+type+'.json',
-      method:'get'
-    })
-    .then(function(res){
-      console.log('获取数据成功');
-      callBack(res);
-    },function(err){
-      console.log(err);
-    });
-  };
-  return service;
-}]);
+// app.factory('commonService',['$http',function($http){
+//   var service={};
+//   service.getDate=function(type,callBack){
+//     $http({
+//       url:'/dangdang-angularjs/data/book_'+type+'.json',
+//       method:'get'
+//     })
+//     .then(function(res){
+//       console.log('获取数据成功');
+//       callBack(res);
+//     },function(err){
+//       console.log(err);
+//     });
+//   };
+//   return service;
+// }]);
